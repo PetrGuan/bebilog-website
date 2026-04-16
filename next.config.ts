db6c9c8d545ecb67f5e8470300c5d788cdd322e8
Peter Guan <jungua@microsoft.com>
2026-04-16 08:50:22 -0700

    fix: disable Next.js image optimization for static export

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
